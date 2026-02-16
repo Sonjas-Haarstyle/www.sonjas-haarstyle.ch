07399bfd01a43246cf34e96c209774faab914976
jku <jan@jku.one>
2026-02-16 14:07:25 +0100

    gallery reimported with thumbnail II

--- thumbnail.sh
+++ thumbnail.sh
@@ -2,11 +2,13 @@
 
 base_path=./static/pic/gallery
 
+height=300
+width=130
+
 for file in "$base_path"/pics/*; do
     if [ -e "$file" ]; then
-
         filename=$(basename "$file")
         echo "create thumbnail for $filename"
-        magick "$file" -resize 300x120 "$base_path"/thumbnail/"$filename"
+        magick "$file" -resize "$height"x"$width" "$base_path"/thumbnail/"$filename"
     fi
 done
